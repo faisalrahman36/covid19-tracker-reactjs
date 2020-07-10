c2228f6ed447e115bb96519ed522f3575799fc00
faisalrahman36@hotmail.com <faisalrahman36@hotmail.com>
2020-07-10 23:22:28 +0500

    covid tracker app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import React,  {useEffect,useState} from 'react';
+import React from 'react';
 import './App.css';
 import Header from './Header';
 
-import MenuIcon from '@material-ui/icons/Menu';
+//import MenuIcon from '@material-ui/icons/Menu';
 
-import { Button, Icon, makeStyles, Grid, IconButton, AppBar, Toolbar, Typography } from '@material-ui/core'
+import {makeStyles, AppBar } from '@material-ui/core'
 
-import {Bar} from 'react-chartjs-2';
+//import {Bar} from 'react-chartjs-2';
 import CovGrid from './CovGrid';
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  testButton : {
-    backgroundColor : "red"
-  },
-  back : {
-
-  //backgroundImage: 
-
-
-  }
-}));
 
 function App() {
 
--- src/GlobalData.js
+++ src/GlobalData.js
@@ -1,7 +1,7 @@
 import React, {useEffect,useState} from 'react';
 //import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import { Button, Icon, makeStyles, Grid, IconButton, AppBar, Toolbar, Typography } from '@material-ui/core'
+import {  makeStyles, Typography } from '@material-ui/core'
 import { yellow } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
@@ -24,27 +24,38 @@ export default function GlobalData() {
   const classes = useStyles();
   
   const [globalsummary,setGlobalsummary]=useState();
-
+  const [isdataloading,setdataloading]=useState(false);
   useEffect(() => {
    
     async function fetchGlobalSummary() {
-
+   setdataloading(true);
    const apiResponse = await fetch('https://api.thevirustracker.com/free-api?global=stats');
    console.log(apiResponse);
    let globaldataResponse=await apiResponse.json() 
    console.log(globaldataResponse);
    setGlobalsummary(globaldataResponse);
-
+   setdataloading(false)
     }
 
     fetchGlobalSummary();
 
-  })
+  },[])
 
+  if(isdataloading){
+    return <div>Loading  data ...
+    <br/> Meanwhile you can read these guidelines for Covid 19:
+    <br />
+
+    <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019" target="_blank">WHO portal for COVID 19</a>
+    <br />
+    <a href="http://covid.gov.pk/guideline" target="_blank">Government of Pakistan guidelines for COVID19</a>  
+
+</div>
+   }
 
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper} variant="outlined" square>
+             <Paper className={classes.paper} variant="outlined" square>
           <div className={Typography.root}>
      <Typography variant="h4" gutterBottom style={{color: 'green'}}>
          {globalsummary && globalsummary.results && globalsummary.results[0].total_cases}
@@ -115,6 +126,7 @@ export default function GlobalData() {
 </Typography>
 </div>
 </Paper>
+ 
     </div>
   );
 }
